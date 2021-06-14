06c84d67ea346bbb3241eb293756b42e64a1374c
root <root@kube.pulseheberg.com>
2021-06-14 05:58:16 +0200

    feat: update config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # [Automation of Everything - How To Combine Argo Events, Workflows & Pipelines, CD, and Rollouts](https://youtu.be/XNXJtxkUKeY)
+
+https://gist.github.com/kconde2/522e39d2b5696cc6433e724fd829cc01
+https://blog.stack-labs.com/code/kustomize-101/
+
+- `kubeseal < mysecret.yaml -o yaml > sealedsecret.yaml`
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 k apply -k argo-cd/overlays/production
 kubens
 m enable nginx
-microk8s enable ingress\n
+microk8s enable ingress
 
 
 k apply -k argo-cd/overlays/production
@@ -9,16 +9,15 @@ k apply -k argo-cd/overlays/production
 
 
 
-microk8s enable ingress\n
+microk8s enable ingress
 k9s
 kubens
 kubectl --namespace argocd get secret argocd-initial-admin-secret --output jsonpath="{.data.password}" | base64 --decode
 export PASS=$(kubectl --namespace argocd get secret argocd-initial-admin-secret --output jsonpath="{.data.password}" | base64 --decode)
 argocd login --insecure --username admin --password $PASS --grpc-web argo.kube.kabaconde.com
-argo login --insecure --username admin --password $PASS --grpc-web argo.kube.kabaconde.com
 VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
-curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64\n
-chmod +x /usr/local/bin/argocd\n
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
 argocd account update-password --current-password $PASS --new-password admin
 
 # kubectl --namespace argocd rollout status deploymnet argocd-server
